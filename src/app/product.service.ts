a2591819ea3ec3d79b1d85485e565ffae650173c
Sohail Ali <sohail.ali@fdmgroup.com>
2020-11-17 22:25:32 -0500

    Generate and import Product Service

---
+++ src/app/product.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+@Injectable()
+export class ProductService {
+
+  constructor(private _http: Http) { }
+
+}
+
+_albumUrl = "../assets/album.json";
