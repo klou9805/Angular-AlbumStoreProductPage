9f67e04e6305b77069541e2acafb78976e530499
Sohail Ali <sohail.ali@fdmgroup.com>
2020-11-19 07:12:23 -0500

    Modify getAlbum method

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,9 +3,12 @@ import { Http, Response} from '@angular/http';
 
 @Injectable()
 export class ProductService {
-
+  _albumUrl = "../assets/album.json";
   constructor(private _http: Http) { }
 
+  getAlbum(id: number){
+    return this._http.get(this._albumUrl);
+  }
 }
 
-_albumUrl = "../assets/album.json";
+
